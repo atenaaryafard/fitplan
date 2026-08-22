2e5625e8173e24f45e765024ea543b7a2cc2861f
atenaaryafard <atenaaryafard3@gmail.com>
2026-08-22 02:33:42 -0800

    Update app.py

--- app.py
+++ app.py
@@ -760,7 +760,10 @@ def export_program_pdf():
 # START
 # =========================================================
 
+init_db()
+
 if __name__ == "__main__":
     init_db()
     port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
