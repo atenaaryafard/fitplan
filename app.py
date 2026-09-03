a53c10fce479a2feb04165ea18f22c51ca4fb64e
atenaaryafard <atenaaryafard3@gmail.com>
2026-09-03 03:15:19 -0800

    Update app.py

--- app.py
+++ app.py
@@ -918,7 +918,7 @@ def save_program():
         INSERT INTO programs
         (coach_id, athlete_name, athlete_age, athlete_height, athlete_weight,
          athlete_goal,athlete_gender,sizes, program_name, program_data, notes, created_at)
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
     """, (
         coach["id"],
         data.get("athlete_name", ""),
