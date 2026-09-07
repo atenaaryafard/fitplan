7524ab0b413a4a1138217dc8e4578745f204e97c
atenaaryafard <atenaaryafard3@gmail.com>
2026-09-07 06:16:13 -0800

    Update app.py

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ def init_db():
         CREATE TABLE IF NOT EXISTS coaches (
             id SERIAL PRIMARY KEY,
             name TEXT NOT NULL,
-            email TEXT UNIQUE
+            phone TEXT UNIQUE NOT NULL,
             password TEXT NOT NULL,
 
             is_admin BOOLEAN DEFAULT FALSE,
