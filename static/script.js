cea432dc56b51a31313e291a0ab154abaf9f97ac
atenaaryafard <atenaaryafard3@gmail.com>
2026-09-08 01:45:32 -0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -144,34 +144,6 @@ const DAY_DEFS = [
 ];
 
 
-/* =====================================================
-   EXERCISE DATABASE
-===================================================== */
-
-const exercises = [
-
-    { name: "پرس سرشانه باربل", muscle: "سرشانه", gif: "" },
-    { name: "لانج دمبل", muscle: "چهارسر ران", gif: "https://fa.pelank.com/wp-content/uploads/2026/07/dumbbell-lunges.gif" },
-    { name: "استپ آپ", muscle: "چهارسر ران", gif: "https://cdnfa.ir/mokamelshope/8ed3/uploads/step-up8.gif" },
-    { name: "پرس سینه دمبل", muscle: "سینه", gif: "https://cdnfa.ir/mokamelshope/8ed3/uploads/incline-dumbbell-bench-press1.gif" },
-    { name: "پرس بالا سینه دمبل", muscle: "سینه", gif: "https://fa.pelank.com/wp-content/uploads/2026/02/Dumbbell-Reverse-Grip-30-Degrees-Incline-Bench-Press.gif" },
-    { name: "فلای سینه دستگاه", muscle: "سینه", gif: "https://morabihamrah.com/wp-content/uploads/2023/10/Pec-Deck-Fly12.gif" },
-    { name: "پوش آپ", muscle: "سینه", gif: "https://routination.com/wp-content/uploads/2023/11/exercises-chest7.gif" },
-    { name: "ددلیفت دمبل", muscle: "همسترینگ", gif: "https://fitnessia.ir/wp-content/uploads/2023/09/dumbbell-deadlift.gif" },
-    { name: "قایقی دست باز", muscle: "پشت", gif: "https://fitnessia.ir/wp-content/uploads/2023/09/wide-grip-row.gif" },
-    { name: "پرس سرشانه دمبل", muscle: "سرشانه", gif: "https://cdnfa.ir/mokamelshope/8ed3/uploads/dumbbell-shoulder-press-gif.gif" },
-    { name: "نشر جانب", muscle: "سرشانه", gif:"https://cdnfa.ir/mokamelshope/8ed3/uploads/leg/1-nashr-janb-dmbl-aistadh.gif" },
-    { name: "نشر خم", muscle: "سرشانه", gif:"https://fa.pelank.com/wp-content/uploads/2025/01/Bent-Over-Lateral-Raise.gif" },
-    { name: " جلو بازو دمبل لاری", muscle: "جلو بازو", gif:"https://routination.com/wp-content/uploads/2023/11/exercises-biceps23.gif" },
-    { name: "پشت بازو دمبل", muscle: "پشت بازو" },
-    { name: "پشت بازو بالای سر", muscle: "پشت بازو", gif: "https://morabihamrah.com/wp-content/uploads/2023/09/Dumbbell-Triceps-Extension12.gif" },
-    { name: "پل باسن", muscle: "باسن", gif: "https://fa.pelank.com/wp-content/uploads/2026/07/Barbell-Hip-Thrust.gif" },
-    { name: "ساق پا", muscle: "ساق", gif: "https://morabihamrah.com/wp-content/uploads/2023/07/Dumbbell-Calf-Raise123.gif" },
-    { name: "کرانچ", muscle: "شکم", gif: "https://mojekooh.com/wp-content/uploads/2023/09/crunch.gif" },
-    { name: "پلانک", muscle: "شکم", Image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqoBxh0AilvQIxLFgT1rjRF42cBcA3419_ular6qOTaQ&s=10" },
-
-];
-
 
 /* =====================================================
    PAGE LOAD
