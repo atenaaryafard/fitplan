33eba1373efb0a0ee4dc79c99c9e69e60c93b462
atenaaryafard <atenaaryafard3@gmail.com>
2026-09-09 06:46:54 -0800

    Update app.py

--- app.py
+++ app.py
@@ -78,21 +78,7 @@ def get_plan(plan_id):
         plan = conn.execute("""
             SELECT * FROM plans WHERE id = ?
         """, (plan_id,)).fetchone()
-    
-        cursor.execute("""
-        UPDATE plans
-        SET price = CASE plan_key
-    
-            WHEN 'basic' THEN '1,980,000 تومان'
-    
-            WHEN 'branded' THEN '4,750,000 تومان'
-    
-            WHEN 'premium' THEN '7,950,000 تومان'
-    
-        END
-    """)
-    
-    conn.commit()
+
 
     conn.close()
 
@@ -271,6 +257,18 @@ def init_db():
              NULL, NULL, TRUE, TRUE, 5, 5)
         """)
 
+        conn.commit()
+
+         cursor.execute("""
+            UPDATE plans
+            SET price = CASE plan_key
+                WHEN 'basic' THEN '1,980,000 '
+                WHEN 'branded' THEN '4,750,000 '
+                WHEN 'premium' THEN '7,950,000'
+                ELSE price
+            END
+         """)
+    
         conn.commit()
 
     conn.close()
